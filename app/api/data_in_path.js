bb29c1ca5ded6434443536da666a2e449ffb87fd
ppz <372301467@qq.com>
2023-05-09 16:27:13 +0800

    feat: api demo "data in body"

--- app/api/data_in_path.js
+++ app/api/data_in_path.js
@@ -1,19 +1,19 @@
 const { simple } = require('./_base')
 
-// 取出请求里的数据
+// 取出请求路径里的数据
 module.exports = simple(
   {
-    prefix: '/data_in_request'
+    prefix: '/data_in_path'
   },
   router => {
-    // 取路径参数（用问号隔开的那种）
+    // 1. 用问号隔开的那种
     // GET /data_in_request/query?name=ppz&year=3
     router.get('/query', ctx => {
       console.log('query', ctx.request.query)
       ctx.respond.json(ctx.request.query)
     })
     
-    // 取路径参数（在路径里的那种）
+    // 2. 在路径里的那种
     // GET /data_in_request/params/ccz/2
     router.get('/params/:name/:year', ctx => {
       console.log('params', ctx.request.params)
